fde2ff3c41e97afa2928babbe1d29f2d89929a94
Rodacay <rodaca97@hotmail.com>
2023-09-21 09:27:59 -0500

    feat: se crea empoins para los primero 7 puntos
    hay que revisar el 7 punto si carga y agrupa los datos pero no muestra los datos de la otra tabla

--- backend/routes/rutas.js
+++ backend/routes/rutas.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { MongoClient } from 'mongodb';
+import { MongoClient,ObjectId } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -10,6 +10,10 @@ const client = new MongoClient(process.env.MONGO_URI);
 const db = client.db('Reserva_Coches');
 const alquiler = db.collection('alquiler');
 const cliente = db.collection('cliente');
+const reserva = db.collection('reserva');
+const empleado = db.collection('empleado');
+const sucursal = db.collection('sucursal');
+const sucursal_automovil=db.collection('sucursal_automovil');
 
 /* Mostrar todos los clientes registrados en la base de datos. */
 router.get('/cliente/all', async (req, res)=>{
@@ -24,7 +28,7 @@ router.get('/cliente/all', async (req, res)=>{
 });
 
 //Obtener todos los automóviles disponibles para alquiler.
-router.get('/alquiler/filtroDisponible', async (req, res)=>{
+router.get('/alquiler/filtro/Disponible', async (req, res)=>{
     try {
         await client.connect();
         const result = await alquiler.aggregate([
@@ -64,9 +68,8 @@ router.get('/alquiler/filtroDisponible', async (req, res)=>{
     }
 });
 
-/* Listar todos los alquileres activos junto con los datos de los
-clientes relacionados. */
-router.get('/alquiler/filtroPendiente', async (req, res)=>{
+/* Listar todos los alquileres activos junto con los datos de los clientes relacionados. */
+router.get('/alquiler/filtro/Pendiente', async (req, res)=>{
     try {
         await client.connect();
         const result = await alquiler.aggregate([
@@ -110,6 +113,140 @@ router.get('/alquiler/filtroPendiente', async (req, res)=>{
 });
 
 /* Mostrar todas las reservas pendientes con los datos del cliente y el automóvil reservado. */
+router.get('/reserva/filtro/Pendiente', async (req, res)=>{
+  try {
+      await client.connect();
+      const result = await reserva.aggregate([
+        {
+          $match: { Estado: "Pendiente" } // Filtra las reservas pendientes
+        },
+        {
+          $lookup: {
+            from: "cliente",
+            localField: "ID_Cliente",
+            foreignField: "_id",
+            as: "cliente"
+          }
+        },
+        {
+          $unwind: "$cliente" // Desnormaliza el resultado del $lookup de clientes
+        },
+        {
+          $lookup: {
+            from: "automovil",
+            localField: "ID_Automovil",
+            foreignField: "_id",
+            as: "automovil"
+          }
+        },
+        {
+          $unwind: "$automovil" // Desnormaliza el resultado del $lookup de automoviles
+        },
+        {
+          $project: {
+            _id: 0,
+            "cliente.Nombre": 1,
+            "cliente.Apellido": 1,
+            "cliente.CC": 1,
+            "cliente.Tipo": 1,
+            "cliente.Direccion": 1,
+            "cliente.Telefono": 1,
+            "cliente.Email": 1,
+            "automovil.Marca": 1,
+            "automovil.Modelo": 1,
+            "automovil.Anio": 1,
+            "automovil.Tipo": 1,
+            "automovil.Capacidad": 1,
+            "automovil.Precio_Diario": 1,
+          }
+        }
+      ]).toArray();
+      res.json(result)
+      client.close();
+  } catch (error) {
+      res.status(404).json({message: error.message});
+  }
+});
+
+/* Obtener los detalles del alquiler con el ID_Alquiler específico. */
+router.get('/alquiler/filtro/:id', async (req, res)=>{
+  try {
+      const id = req.params.id;
+      await client.connect();
+      const result = await alquiler.find({ _id: new ObjectId(id) }).toArray();
+      res.json(result)
+      client.close();
+  } catch (error) {
+      res.status(404).json({message: error.message});
+  }
+});
+
+/* Listar los empleados con el cargo de "Vendedor" */
+router.get('/empleado/filtro/Vendedor', async (req, res)=>{
+  try {
+      await client.connect();
+      const result = await empleado.find({ Cargo: "Vendedor" }).toArray();
+      res.json(result)
+      client.close();
+  } catch (error) {
+      res.status(404).json({message: error.message});
+  }
+});
+
+/* Mostrar la cantidad total de automóviles disponibles en cada sucursal. */
+router.get('/sucursal_automovil/total/automovil', async (req, res)=>{
+  try {
+      await client.connect();
+      const result = await sucursal_automovil.aggregate([
+        {
+          $group: {
+            _id: "$ID_Sucursal",
+            totalAutomoviles: { $sum: "$Cantidad_Disponible" }
+          }
+        },
+        {
+          $lookup: {
+            from: "sucursal",
+            localField: "ID_Sucursal",
+            foreignField: "_id",
+            as: "sucursal"
+          }
+        },
+        {
+          $unwind: "$sucursal"
+        },
+        {
+          $project: {
+            totalAutomoviles:1
+          }
+        }
+      ]).toArray();
+      res.json(result)
+      client.close();
+  } catch (error) {
+      res.status(404).json({message: error.message});
+  }
+});
+
+
+
+
+
+
+
+
 
 
 export default router;
+/* 
+router.get('/tabla/filtro/', async (req, res)=>{
+    try {
+        await client.connect();
+        const result = await tabla.find().toArray();
+        res.json(result)
+        client.close();
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+});
+ */
